f0e9c896ef840ec52852d92d05ff14dbcc1b34a8
izaxveta <izavetha@gmail.com>
2018-02-05 15:24:19 -0700

    Route for foods show fully functional.

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -15,4 +15,16 @@ router.get('/', (req, res, next) => {
   })
 })
 
+router.get('/:id', (req, res, next) => {
+  let id = req.params.id
+
+  database.raw('SELECT * FROM foods WHERE foods.id = ?', id)
+    .then(food => {
+      if(!food) {
+        return res.sendStatus(404)
+      } else {
+        res.json(food.rows[0])
+      }
+    })
+})
 module.exports = router
