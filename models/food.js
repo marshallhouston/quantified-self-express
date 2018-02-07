68c4570428aac8c705fde7b1d90ff28d2c19636c
Marshall Houston <marshall.houston@gmail.com>
2018-02-07 09:49:25 -0700

    create update method on food model

--- models/food.js
+++ models/food.js
@@ -26,7 +26,16 @@ const Food = {
     ).then(food => {
       return food.rows[0]
     })
-  }
+  },
+
+  update: (id, name, calories) => {
+    return database.raw(
+      'UPDATE foods SET name = ?, calories = ? WHERE id = ? RETURNING *',
+      [name, calories, id]
+    ).then(food => {
+      return food.rows[0]
+    })
+  },
 }
 
 
