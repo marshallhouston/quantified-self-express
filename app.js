062597f81cb8d2fe3a34253c6ac5e971d18fba22
Marshall Houston <marshall.houston@gmail.com>
2018-02-07 15:30:22 -0700

    create routes file for /api/v1/meals

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var foods = require('./routes/api/v1/foods')
+var meals = require('./routes/api/v1/meals')
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/api/v1/foods', foods);
+app.use('/api/v1/meals', meals);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
