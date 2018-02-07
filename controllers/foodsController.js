2e392caf8acd215feff53fae787ad86c5536c6ca
Marshall Houston <marshall.houston@gmail.com>
2018-02-07 09:48:58 -0700

    create update method in foodsController

--- controllers/foodsController.js
+++ controllers/foodsController.js
@@ -47,4 +47,35 @@ const create = (req, res, next) => {
     })
 }
 
-module.exports = {index, show, create}
+const update = (req, res, next) => {
+
+  let food = req.body.food
+  if (!food) {
+    return res.status(400).send({
+      error: "Unsuccessful update. The format should be { food : { name : foodName , calories : foodCalories } }"
+    })
+  }
+
+  let name = food.name
+  let calories = parseInt(food.calories)
+
+  if(!name || !calories) {
+    return res.status(400).send({
+      error: "Unsuccessful update. Name and calories are required fields. { food : { name : foodName , calories : foodCalories } }"
+    })
+  }
+
+  let id = req.params.id
+  if(!id) {
+    return res.status(400).send({
+      error: "Unsuccessful update."
+    })
+  }
+
+  Food.update(id, name, calories)
+    .then( food => {
+      res.status(200).json(food)
+    })
+}
+
+module.exports = {index, show, create, update}
