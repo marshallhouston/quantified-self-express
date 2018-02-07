766dac76e159462730900d625ba91b61e9d816ef
izaxveta <izavetha@gmail.com>
2018-02-06 18:00:12 -0700

    Pull database calls out into model.

---
+++ models/food.js
@@ -0,0 +1,33 @@
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('../knexfile')[environment]
+const database = require('knex')(configuration)
+
+const Food = {
+  findAll: () => {
+    return database.raw(
+      'SELECT * FROM foods'
+    ).then(foods => {
+      return foods.rows
+    })
+  },
+
+  find: (id) => {
+    return database.raw(
+      'SELECT * FROM foods WHERE foods.id = ?', id
+    ).then(food => {
+      return food.rows[0]
+    })
+  },
+
+  create: (name, calories) => {
+    return database.raw(
+      'INSERT INTO foods (name, calories) VALUES (?, ?) RETURNING *',
+      [name, calories]
+    ).then(food => {
+      return food.rows[0]
+    })
+  }
+}
+
+
+module.exports = Food
