513a50d6890ba58e1f63f8f9a00e8db1d3b1ef6f
izaxveta <izavetha@gmail.com>
2018-02-08 14:07:19 -0700

    Remove commented out pry from mealFood.js model

--- models/mealFood.js
+++ models/mealFood.js
@@ -29,7 +29,6 @@ const MealFood = {
       'DELETE FROM meal_foods WHERE meal_foods.meal_id = ? AND meal_foods.food_id = ? RETURNING *',
       [mealId, foodId]
     ).then((response) => {
-      // var pry = require('pryjs'); eval(pry.it)
       return database.raw('SELECT meals.name FROM meals WHERE meals.id = ? UNION SELECT foods.name FROM foods WHERE foods.id = ?',
       [mealId, foodId]
     ).then(mealAndFoodNames => {
