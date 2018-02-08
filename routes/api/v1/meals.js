267430952b2f89683cad2649deb2ba0f093d0040
Marshall Houston <marshall.houston@gmail.com>
2018-02-07 17:23:35 -0700

    add route for and mealFoodsController to create new mealFood

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const mealsController = require('../../../controllers/mealsController')
+const mealFoodsController = require('../../../controllers/mealFoodsController')
 
 router.get('/', mealsController.index)
 
 router.get('/:id/foods', mealsController.show)
+
+router.post('/:mealId/foods/:foodId', mealFoodsController.create)
 
 module.exports = router
