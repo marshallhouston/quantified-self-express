7654230bdf2bc60e4419a88313da6698a34df9c0
izaxveta <izavetha@gmail.com>
2018-02-07 17:28:32 -0700

    Meal model find method obtains meal object with foods attribute containing all foods associated with meal.

--- models/meal.js
+++ models/meal.js
@@ -10,6 +10,15 @@ const Meal = {
       return meals.rows
     })
   },
+
+  find: (id) => {
+    return database.raw(
+      'SELECT m.*, json_agg(f.*) AS foods FROM meals m INNER JOIN meal_foods mf ON m.id = mf.meal_id INNER JOIN foods f ON mf.food_id = f.id where m.id = ? GROUP BY m.id',
+      [id]
+    ).then(meal => {
+      return meal.rows[0]
+    })
+  }
 }
 
 module.exports = Meal
