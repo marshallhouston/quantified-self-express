1b25480a94c09324419cdd6af9cf073620b66bc2
izaxveta <izavetha@gmail.com>
2018-02-08 15:42:46 -0700

    Configure CORS settings everywhere.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -23,6 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
+app.options('*', cors())
 
 app.use('/', index);
 app.use('/users', users);
