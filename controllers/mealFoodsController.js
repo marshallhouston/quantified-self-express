6e9ef62bb87c3ef1a836b149036a5ffdffa9d325
Marshall Houston <marshall.houston@gmail.com>
2018-02-07 17:24:07 -0700

    create mealFood controller with dynamic message response

---
+++ controllers/mealFoodsController.js
@@ -0,0 +1,21 @@
+const MealFood = require('../models/mealFood')
+
+const create = (req, res, next) => {
+  let mealId = req.params.mealId
+  let foodId = req.params.foodId
+
+  MealFood.create(mealId, foodId)
+  .then(mealFood => {
+    if(!mealFood) {
+      return res.status(404)
+    } else {
+      res.status(201).send({
+        message: `Successfully added ${mealFood.food_name} to ${mealFood.meal_name}`
+      })
+    }
+  })
+}
+
+module.exports = {
+  create,
+}
